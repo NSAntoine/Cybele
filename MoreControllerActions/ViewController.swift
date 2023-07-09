9b0be32652da71bbc7980f523d2245282daff33f
Serena <osama.id78@gmail.com>
2023-07-09 22:59:28 +0300

    Initial Commit

---
+++ MoreControllerActions/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MoreControllerActions
+//
+//  Created by Serena on 09/07/2023.
+//  
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
